1d0daffde131c1960be743f66bf8a47d9b87e1d0
Will Simmons <willsimmons@gmail.com>
2015-03-11 14:49:07 -0700

    redo database for dashMe

--- db/schema.rb
+++ db/schema.rb
@@ -16,13 +16,6 @@ ActiveRecord::Schema.define(version: 20150301201716) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "articles", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "provider"
     t.string   "uid"
