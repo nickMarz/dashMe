55c2f11d164266b6de5781a8bd88cbdb1a193cb9
Will Simmons <willsimmons@gmail.com>
2015-03-11 15:44:13 -0700

    oauth migration added to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150301201716) do
+ActiveRecord::Schema.define(version: 20150311224129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,10 @@ ActiveRecord::Schema.define(version: 20150301201716) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "oauth_token"
+    t.string   "oauth_secret"
   end
 
 end
