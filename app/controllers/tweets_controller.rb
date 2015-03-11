ac8b199b31447cc569ddd519323ad77e9d7e0101
Nick Marazzo <designmarz@gmail.com>
2015-03-11 14:34:22 -0700

    workign on send tweet function

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,14 @@
+class TweetsController < ApplicationController
+  def new
+  end
+
+  def create
+    current_user.tweet(twitter_params[:message])
+    redirect_to root_url, notice: "Tweet sent!"
+  end
+
+  def twitter_params
+    params.require(:tweet).permit(:message)
+  end
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,15 @@ class User < ActiveRecord::Base
       user.name = auth["info"]["nickname"] ? auth["info"]["nickname"] : auth["info"]["name"]
     end
   end
+
+  def tweet(tweet)
+  client = Twitter::REST::Client.new do |config|
+    config.consumer_key        = Rails.application.config.twitter_key
+    config.consumer_secret     = Rails.application.config.twitter_secret
+    config.access_token        = oauth_token
+    config.access_token_secret = oauth_secret
+    end
+  client.update(tweet)
+  end
+
 end
