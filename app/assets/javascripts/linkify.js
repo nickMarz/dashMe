47f8ff68a4bf3d76ea0ac14294c42c8078b2abb6
Nick Marazzo <designmarz@gmail.com>
2015-03-13 10:04:04 -0700

    added delay to linkify

--- app/assets/javascripts/linkify.js
+++ app/assets/javascripts/linkify.js
@@ -1,6 +1,11 @@
 $(document).ready(function () {
+setTimeout(function() {
   $('p').linkify();
   console.log('linkify')
+}, 5000);
+// Do something after 5 seconds
+
+  
 });
 
 // $(window).on('load', 
