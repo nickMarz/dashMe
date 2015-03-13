cc18f38915a939179ef9dab3fe4823992b9f1baf
Will Simmons <willsimmons@gmail.com>
2015-03-12 20:25:15 -0700

    most recent tweet is displayed

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
   function getTweets() {
     $.getJSON("/tweets/show",function (data) {
-      var list = $("timeline").empty();
-      for (var i = 0; i < data.length; i++) {
-        console.log(data[i].text);
+      var list = $("p.tweets").empty();
+      for (var i = 20; i < 21; i++) { 
+        list.html("<img src='"+data[i].user.profile_image_url+"'>"+data[i].text+"by: "+data[i].user.name+"</p>");
       }
     });
   } 
   getTweets();
   setInterval (getTweets, 120000); 
 });
+
