72ae290dcac4dd20970ab33504e8bfa9668cccd5
Will Simmons <willsimmons@gmail.com>
2015-03-12 22:28:16 -0700

    one tweet displaying

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -2,12 +2,13 @@ $(document).ready(function() {
   function getTweets() {
     $.getJSON("/tweets/show",function (data) {
       var list = $("p.tweets").empty();
-      for (var i = 20; i < 21; i++) { 
-        list.html("<img src='"+data[i].user.profile_image_url+"'>"+data[i].text+"by: "+data[i].user.name+"</p>");
+      for (var i = 0; i < Things.length; i++) {
+        list.html("<img src='"+data[i].user.profile_image_url+"'>");
+        list.append(data[i].text+"by: "+data[i].user.name);
+      // $('p.tweets').linkify(); somthing is up with the linkify javascript?
       }
     });
   } 
   getTweets();
   setInterval (getTweets, 120000); 
 });
-
