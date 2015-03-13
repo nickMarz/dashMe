fa8e5ea93ab512caaa6c5808b75e3f0f39a7d2e4
Nick Marazzo <designmarz@gmail.com>
2015-03-13 10:43:09 -0700

    still working on todo

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -3,7 +3,7 @@ $('.todoForm').submit(function (e) {
     e.preventDefault();
     if ($('.todosubmit').val() !== '') {
         var todosubmit_value = $('.todosubmit').val();
-        $('ul').append('<li class="todoItem">' + todosubmit_value + '<a href=" " class="deleteX" id="deleteX">x</a></li>');
+        $('ul').append('<li class="todoItem">' + todosubmit_value + '<a href="#" class="deleteX" id="deleteX">x</a> </li>');
     }
     $('.todosubmit').val('');
     return false;
