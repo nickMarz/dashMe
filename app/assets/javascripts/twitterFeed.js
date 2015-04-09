8743943e5fa27adaf5e846405b815fd8455f22f9
Nick Marazzo <nick@designmarz.com>
2015-04-09 13:19:05 -0700

    limited for loop on tweet data to 5, upped pol rate, added console.log of data

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -1,14 +1,17 @@
+
 $(document).ready(function() {
   function getTweets() {
     $.getJSON("/tweets/show",function (data) {
       var list = $("p.tweets").empty();
-      for (var i = 0; i < data.length; i++) {
+      // for (var i = 0; i < data.length; i++)
+      for (var i = 0; i < 5; i++) {
         list.append("<div class='twitt'><img src='"+data[i].user.profile_image_url+"' class='twitPic'>");
         list.append(data[i].text+" BY: "+data[i].user.name+"</div><br>");
       // $('p.tweets').linkify(); somthing is up with the linkify javascript?
+      console.log(data);
       }
     });
   } 
   getTweets();
-  setInterval (getTweets, 120000); 
+  setInterval (getTweets, 100000); 
 });
