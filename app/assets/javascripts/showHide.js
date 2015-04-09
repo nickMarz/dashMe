860a09122250f09018a6bfd08a257c9c08ed4cf9
Nick Marazzo <designmarz@gmail.com>
2015-04-09 14:58:52 -0700

    fixed personal bio's, added github logo and link to each individual's github

--- app/assets/javascripts/showHide.js
+++ app/assets/javascripts/showHide.js
@@ -1,11 +1,19 @@
 $(document).ready(function(){
 
-
+// About dev's show hide function
 $(".slidingDiv").hide();
 	$(".show_hide").show();
 	
 	$('.show_hide').click(function(){
 	$(".slidingDiv").slideToggle();
 	});
+
+$(".optionsDiv").hide();
+  $(".show_hide_opt").show();
+  
+  $('.show_hide_opt').click(function(){
+  $(".optionsDiv").slideToggle();
+  });
+
 
 });
