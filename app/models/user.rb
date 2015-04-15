28406ea7e10eb189ce91d401cc5d9c0328f6e57c
Nick Marazzo <designmarz@gmail.com>
2015-04-15 12:42:21 -0700

    fixed authentiction

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,14 +28,13 @@ class User < ActiveRecord::Base
       config.access_token        = oauth_token
       config.access_token_secret = oauth_secret
       end
-      binding.pry
     client.update(tweet)
   end
 
   def feed #experimental
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = Rails.application.config.twitter_key
-      config.consumer_secret     = Rails.application.config.twitter_secret
+      config.consumer_key        = ENV["twitter_key"]
+      config.consumer_secret     = ENV["twitter_secret"]
       config.access_token        = oauth_token
       config.access_token_secret = oauth_secret
       end
