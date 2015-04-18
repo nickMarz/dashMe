122891cd04827f0566b0174613df1418985d60c2
Nicholas Marazzo <designmarz@gmail.com>
2015-04-17 20:36:16 -0700

    somehting broke (on heroku)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,19 +2,19 @@ class User < ActiveRecord::Base
   # def self.from_omniauth(auth)
   #   where(provider: auth.provider, uid: auth.uid).first || create_from_omniauth(auth)
   # end
-def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
-    data = access_token.info
-    user = User.where(:email => data["email"]).first
+# def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+#     data = access_token.info
+#     user = User.where(:email => data["email"]).first
 
-    # Uncomment the section below if you want users to be created if they don't exist
-    # unless user
-    #     user = User.create(name: data["name"],
-    #        email: data["email"],
-    #        password: Devise.friendly_token[0,20]
-    #     )
-    # end
-    user
-end
+#     # Uncomment the section below if you want users to be created if they don't exist
+#     # unless user
+#     #     user = User.create(name: data["name"],
+#     #        email: data["email"],
+#     #        password: Devise.friendly_token[0,20]
+#     #     )
+#     # end
+#     user
+# end
 
   def self.from_omniauth(auth)
    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
