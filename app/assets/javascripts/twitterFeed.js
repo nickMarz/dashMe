0b1dd8a99b8d584cec353034cf540ae9fd6b8fcd
Nick Marazzo <designmarz@gmail.com>
2016-02-22 15:49:11 -0800

    attempt at fixing error round 3

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
         var profilePic = data[i].user.profile_image_url;
         var userName = data[i].user.name;
         var userTweet = data[i].text();
-        list.append('<div class="twitt"><img src="'+ profilePic +'" class="twitPic"><p>' userTweet +' BY: '+ userName +'</p></div>');
+        list.append('<div class="twitt"><img src="'+ profilePic +'" class="twitPic"><p>'+ userTweet +' BY: '+ userName +'</p></div>');
       // $('p.tweets').linkify(); somthing is up with the linkify javascript?
       console.log(data);
       }
