9a0f5c343b838e4d5af8066d517214049910500b
Nick Marazzo <designmarz@gmail.com>
2016-02-22 16:57:44 -0800

    updated feed

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -3,19 +3,21 @@ $(document).ready(function() {
     //TODO: add connection if check to handle errors
     $.getJSON("/tweets/show",function (data) {
       var list = $(".tweets");
-      list.innerHTML = "";
+
+      var allTweest = ""
       for (var i = 0; i < 15; i++) {
         var profilePic = data[i].user.profile_image_url;
         var userName = data[i].user.name;
         var userTweet = data[i].text;
         var finishedTweet = '<div class="twitt"><img src="' + profilePic + '" class="twitPic"><p>' + userTweet +'<br> BY: ' + userName + '</p></div>';
         // list.append(finishedTweet);
-        list.innerHTML += finishedTweet;
+        allTweest += finishedTweet;
         // $(".tweets").innerHTML = finishedTweet;
         // $('p.tweets').linkify(); somthing is up with the linkify javascript?
         console.log(data[i]);
         console.log(finishedTweet);
         }
+        list.innerHTML = allTweest;
       $('.tweets').linkify();
     });
 
