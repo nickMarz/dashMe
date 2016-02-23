e02e832f94294aba296c8a4c8f58748c04f78134
Nick Marazzo <designmarz@gmail.com>
2016-02-22 16:19:09 -0800

    attempt at fixing error round 7

--- app/assets/javascripts/twitterFeed.js
+++ app/assets/javascripts/twitterFeed.js
@@ -2,13 +2,16 @@ $(document).ready(function() {
   function getTweets() {
     //TODO: add connection if check to handle errors
     $.getJSON("/tweets/show",function (data) {
-      var list = $(".tweets").empty();
+      var list = $(".tweets");
+      list.innerHTML = "";
       for (var i = 0; i < 10; i++) {
         var profilePic = data[i].user.profile_image_url;
         var userName = data[i].user.name;
         var userTweet = data[i].text;
         var finishedTweet = '<div class="twitt"><img src="' + profilePic + '" class="twitPic"><p>' + userTweet +'<br> BY: ' + userName + '</p></div>';
-        list.append(finishedTweet);
+        // list.append(finishedTweet);
+        list.innerHTML += finishedTweet;
+        // $(".tweets").innerHTML = finishedTweet;
       // $('p.tweets').linkify(); somthing is up with the linkify javascript?
       console.log(data[i]);
       console.log(finishedTweet);
