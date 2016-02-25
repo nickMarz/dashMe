9d11b7e3cd6f7d16aad9fd89fd808f39766e46b7
Nick Marazzo <designmarz@gmail.com>
2016-02-25 12:12:27 -0800

    added color picker and fixed style error on quote section

---
+++ app/assets/javascripts/app.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('#colorSelector').ColorPicker({
+	color: '#0000ff',
+	onShow: function (colpkr) {
+		$(colpkr).fadeIn(500);
+		return false;
+	},
+	onHide: function (colpkr) {
+		$(colpkr).fadeOut(500);
+		return false;
+	},
+	onChange: function (hsb, hex, rgb) {
+		$('#colorSelector div').css('backgroundColor', '#' + hex);
+		$('.primary_back_color').css('backgroundColor', '#' + hex);
+	}
+});
+
+});
