f8e1674e91b803ec5d6d988ef3e37adace8af7e8
Nick Marazzo <designmarz@gmail.com>
2016-02-29 15:40:11 -0800

    cleaned assets & fixed selector

--- app/assets/javascripts/showHide.js
+++ app/assets/javascripts/showHide.js
@@ -51,9 +51,9 @@ $(document).ready(function(){
     $('#hide_forecast').click(function(e){
       $(".forecast_daily").slideToggle();
       if (true === "matrix(1, 0, 0, 1, 0, 0)" ) {
-        $('#hide_forecast').next().css('transform', 'rotate(180deg)');
+        $('#hide_forecast i').css('transform', 'rotate(180deg)');
       } else {
-        $('#hide_forecast').next().css('transform', 'rotate(0deg)');
+        $('#hide_forecast i').css('transform', 'rotate(0deg)');
       }
   });
 
