7d8e2888254f4989c42f861ce161566d86d9d939
Nick Marazzo <designmarz@gmail.com>
2016-03-02 23:35:14 -0800

    replaced color picker lib, moved options panel

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,38 +1,57 @@
 $(document).ready(function() {
 
+//   $('#colorSelector_backcolor').ColorPicker({
+// 	color: '#0000ff',
+// 	onShow: function (colpkr) {
+// 		$(colpkr).fadeIn(500);
+// 		return false;
+// 	},
+// 	onHide: function (colpkr) {
+// 		$(colpkr).fadeOut(500);
+// 		return false;
+// 	},
+// 	onChange: function (hsb, hex, rgb) {
+// 		$('#colorSelector_backcolor div').css('backgroundColor', '#' + hex);
+// 		$('.primary_back_color').css('backgroundColor', '#' + hex);
+// 	}
+// });
+
+//   $('#colorSelector_textcolor').ColorPicker({
+// 	color: '#ffffff',
+// 	onShow: function (colpkr) {
+// 		$(colpkr).fadeIn(500);
+// 		return false;
+// 	},
+// 	onHide: function (colpkr) {
+// 		$(colpkr).fadeOut(500);
+// 		return false;
+// 	},
+// 	onChange: function (hsb, hex, rgb) {
+// 		$('#colorSelector_textcolor div').css('backgroundColor', '#' + hex);
+// 		$('.light_text_color').css('color', '#' + hex);
+// 	}
+// });
+$('#colorSelector_backcolor').colorpicker({
+      format: 'rgb', // force this format
+      horizontal: true
+    }).on('changeColor', function(ev) {
+    	var color = ev.color.toRGB();
+    	var allColor = 'rgb('+ color.r + ',' + color.g + ',' + color.b + ')';
+    	$('.light_text_color').css('color', allColor )
 
 
-  $('#colorSelector_backcolor').ColorPicker({
-	color: '#0000ff',
-	onShow: function (colpkr) {
-		$(colpkr).fadeIn(500);
-		return false;
-	},
-	onHide: function (colpkr) {
-		$(colpkr).fadeOut(500);
-		return false;
-	},
-	onChange: function (hsb, hex, rgb) {
-		$('#colorSelector_backcolor div').css('backgroundColor', '#' + hex);
-		$('.primary_back_color').css('backgroundColor', '#' + hex);
-	}
-});
+    });
+$('#colorSelector_textcolor').colorpicker({
+      format: 'rgba', // force this format
+      horizontal: true
+    }).on('changeColor', function(ev) {
+    	var color = ev.color.toRGB();
+    	var allColor = 'rgba('+ color.r + ',' + color.g + ',' + color.b + ',' + color.a +')';
+    	$('.primary_back_color').css('backgroundColor', allColor )
 
-  $('#colorSelector_textcolor').ColorPicker({
-	color: '#ffffff',
-	onShow: function (colpkr) {
-		$(colpkr).fadeIn(500);
-		return false;
-	},
-	onHide: function (colpkr) {
-		$(colpkr).fadeOut(500);
-		return false;
-	},
-	onChange: function (hsb, hex, rgb) {
-		$('#colorSelector_textcolor div').css('backgroundColor', '#' + hex);
-		$('.light_text_color').css('color', '#' + hex);
-	}
-});
+
+
+    });
 
 
 $( '#datepicker' ).datepicker();
---
+++ app/assets/javascripts/readme.md
@@ -0,0 +1,10 @@
+on/offline Notes:
+IE(Edge~10) @media query not hiding the show/hide story buttons when viewed on mobil screen (less than 420px)
+
+Online Notes:
+
+
+Offline Notes:
+Clicking in the iFrame on steps 5 & 7 do not advance the slider
+Need to use arrow controls / dropdown / click outside the iframe
+
